be55703d2c34fcdad395e801da73bad63d7e80b2
Juan Pablo Velez Muñoz <juan.velez-mu@mail.escuelaing.edu.co>
2026-04-03 02:01:24 -0500

    FEAT: Construcción controller de torneo/ funcional y con pruebas

--- src/main/java/edu/eci/dosw/techcup/entity/Tournament.java
+++ src/main/java/edu/eci/dosw/techcup/entity/Tournament.java
@@ -1,4 +1,76 @@
 package edu.eci.dosw.techcup.entity;
 
+import java.time.LocalDate;
+
 public class Tournament {
+    //Attributes
+    private long id;
+    private String name;
+    private LocalDate initialDate;
+    private LocalDate finalDate;
+    private int teamsNumber;
+    private double inscriptionCost; //inscription cost for a team
+    private TournamentState state;
+
+    public Tournament(long id, String name) {
+        this.id = id;
+        this.name = name;
+        state = TournamentState.DRAFT;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getInitialDate() {
+        return initialDate;
+    }
+
+    public void setInitialDate(LocalDate initialDate) {
+        this.initialDate = initialDate;
+    }
+
+    public LocalDate getFinalDate() {
+        return finalDate;
+    }
+
+    public void setFinalDate(LocalDate finalDate) {
+        this.finalDate = finalDate;
+    }
+
+    public int getTeamsNumber() {
+        return teamsNumber;
+    }
+
+    public void setTeamsNumber(int teamsNumber) {
+        this.teamsNumber = teamsNumber;
+    }
+
+    public double getInscriptionCost() {
+        return inscriptionCost;
+    }
+
+    public void setInscriptionCost(double inscriptionCost) {
+        this.inscriptionCost = inscriptionCost;
+    }
+
+    public TournamentState getState() {
+        return state;
+    }
+
+    public void setState(TournamentState state) {
+        this.state = state;
+    }
 }
--- src/main/java/edu/eci/dosw/techcup/service/TournamentService.java
+++ src/main/java/edu/eci/dosw/techcup/service/TournamentService.java
@@ -1,5 +1,48 @@
 package edu.eci.dosw.techcup.service;
 
-public class TournamentService {
+import edu.eci.dosw.techcup.entity.Tournament;
+import edu.eci.dosw.techcup.entity.TournamentState;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class TournamentService {
+    private Map<Long, Tournament> tournaments;
+
+    public TournamentService() {
+        this.tournaments = new LinkedHashMap<Long, Tournament>();
+    }
+
+    public boolean createTournament(Tournament tournament) {
+        if(tournament != null && !(tournaments.containsKey(tournament.getId()))) {
+            tournaments.put(tournament.getId(), tournament);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean updateTournament(long tournamentId, Tournament newTournament) {
+        if((tournamentId == newTournament.getId()) && tournaments.containsKey(tournamentId) && (tournaments.get(tournamentId).getState() != TournamentState.ENDED)) {
+            tournaments.put(tournamentId, newTournament);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteTournament(long tournamentId) {
+        if(tournaments.containsKey(tournamentId) && (tournaments.get(tournamentId).getState() == TournamentState.DRAFT)) {
+            tournaments.remove(tournamentId);
+            return true;
+        }
+        return false;
+    }
+
+    public Tournament getTournament(long tournamentId) {
+        return tournaments.get(tournamentId);
+    }
+
+    public Map<Long, Tournament> findAll(){
+        return tournaments;
+    }
 }
