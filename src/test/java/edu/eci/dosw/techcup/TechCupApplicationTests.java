5fa0f5ed44bf18316e7b4c398e4557a5a81c9c7e
Kevin Segura <kevin.svelandia@mail.escuelaing.edu.co>
2026-04-10 17:42:24 -0500

    feat: configure H2 in-memory database for tests

--- src/test/java/edu/eci/dosw/techcup/TechCupApplicationTests.java
+++ src/test/java/edu/eci/dosw/techcup/TechCupApplicationTests.java
@@ -2,8 +2,10 @@ package edu.eci.dosw.techcup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TechCupApplicationTests {
 
 	@Test
